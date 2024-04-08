91e9c28d500a898d7f9453fcc3d2ec7909158402
WallaceWebsBR <wallacealberto@outlook.com.br>
2024-04-08 10:19:23 -0300

    alteração RGC v1.0

--- src/components/app/header/header.jsx
+++ src/components/app/header/header.jsx
@@ -2,8 +2,6 @@ import { h } from 'preact';
 
 import './header.scss';
 
-import GithubLinkButton from './github-link-button';
-
 import OptionsSelect from '../../options-select';
 
 const Header = ({ themes, theme, setTheme, mapIndex, mapList, setMap, views, viewIndex, setViewIndex }) => {
@@ -12,13 +10,12 @@ const Header = ({ themes, theme, setTheme, mapIndex, mapList, setMap, views, vie
 		<div class="header">
 			<div class="container">
 				<div class="group">
-					<span class="title">WC3 DotA Info</span>
+					<span class="title">DotA Wiki - RGC</span>
 				</div>
 				<div class="group">
 					<OptionsSelect label="Version" options={mapVersions} valueIndex={mapIndex || 0} setValueIndex={setMap} disabled={!mapList.length} />
 					<OptionsSelect label="View" options={views} valueIndex={viewIndex} setValueIndex={setViewIndex} />
 					<OptionsSelect label="Theme" options={themes} valueIndex={theme} setValueIndex={setTheme} />
-					<GithubLinkButton />
 				</div>
 			</div>
 		</div>
